a57f72675a677897b417622b9f77d2d3c0d7ad3e
Leyla <c19lk@dalton.org>
2017-08-30 19:32:28 -0400

    updated schedule

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to D-Lab</h2>
         </div>
         <AddSchedules />
         <CheckSchedules />
@@ -19,31 +19,46 @@ class App extends Component {
 
 class AddSchedules extends Component {
   state ={
-    basicarray: ['M', 'T', 'W', 'R', 'F'],
-    array: [[1,2,3,4,5,],[6,7,8,9,10,],[11,12,13,14,15.],[16,17,18,19,20,],[21,22,23,24,25,],[26,27,28,29,30,],[31,32,33,34,35],],
+    basicarray: ['M', 'T', 'W', 'Th', 'F'],
+    array: [['m1','t1','w1','th1','f1',],['m2','t2','w2','th2','f2',],['m3','t3','w3','th3','f3',],['m4','t4','w4','th4','f4',],['m5','t5','w5','th5','f5',], ['m6','t6','w6','th6','f6',],['m7','t7','w7','th7','f7',],],
     myFrees: [],
     newFree: '',
   }
 
-  addFree = (idx) => {
+  addFree = (pidx, didx) => {
      let frees = [...this.state.myFrees]
-     let x = this.state.basicarray[idx]
+     let x = this.state.array[didx][pidx]
      frees.push(x)
 
         this.setState({
           myFrees: frees,
         })
   }
+
   render() {
-    let boxes = this.state.basicarray.map((i, idx) =>{
-      const addFree = () => {
-          this.addFree(idx)
-        }
-        return <span className="allboxes"> <input type="checkbox" className="boxes" onClick={addFree}/> {i}</span>
+    let boxes = this.state.array.map((day, didx) =>{
+        return (
+          <div> 
+            {
+              day.map((period,pidx) =>{
+                const addFree = () => {
+                  this.addFree(pidx, didx)
+                }
+                return <span className="allboxes"> <input type="checkbox" className="boxes" onChange={addFree}/> </span>
+              })
+            }
+          </div>
+        )
       })
+
+    let daysList = this.state.basicarray.map((i) => {
+      return <span className="day-names"> {i} </span>
+    })
     return(
       
         <div>
+        <h4> Select the periods you have free:</h4>
+        {daysList}
         {boxes}
       <p> My free periods are:
       {this.state.myFrees}
