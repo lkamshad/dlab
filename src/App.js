0f5f78280c94dd46840f18c4bee94a933686af9e
Kara Siegel <c19ks@dalton.org>
2017-08-30 16:27:26 -0400

    two components

--- src/App.js
+++ src/App.js
@@ -10,12 +10,27 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddSchedules />
+        <CheckSchedules />
       </div>
     );
   }
 }
 
+class AddSchedules extends Component {
+  render() {
+    return(
+      <h4> Hello </h4>
+    );
+  }
+}
+
+
+class CheckSchedules extends Component {
+  render(){
+    return(
+      <p> Hi </p>
+      );
+  }
+}
 export default App;
