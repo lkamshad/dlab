91f06aa5d7cb2314a3e5df24a23f53c9d779e3d9
Kara Siegel <c19ks@dalton.org>
2017-08-30 18:30:51 -0400

    compare component start

--- src/App.js
+++ src/App.js
@@ -20,15 +20,56 @@ class App extends Component {
 class AddSchedules extends Component {
   render() {
     return(
-      <h4> Hello </h4>
+      <p></p>
     );
   }
 }
 
 class CheckSchedules extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      me: {
+        name: "Kara",
+        freePeriods: ["m3", "m7", "t1", "w4", "th2", "th6"],
+      },
+      otherStudents: [{
+        name: "Leyla",
+        freePeriods: ["m3", "m7", "t3", "t6", "w4", "th2", "th7"],
+      }],
+      commonPeriods: [""]
+    }
+  }
+
+compare = () => {
+  let mySchedule = this.state.me;
+  let friendSchedule = this.state.otherStudents;
+  let common=[];
+  // for (let i = 0; i < mySchedule.length; i++) { 
+  //   for (let j = 0; j < friendSchedule.length; j++) { 
+  console.log(mySchedule)
+  console.log(friendSchedule)
+  if (mySchedule.freePeriods[0]===friendSchedule[0].freePeriods[0]){
+    common.push("hello");
+    console.log("hello")
+  }
+  else{
+    console.log("bye")
+  }
+    //   }
+    // }
+  this.setState({
+    commonPeriods: common,
+  })
+
+}
+
   render(){
     return(
-      <p> Hi </p>
+      <div className="compare-area">
+        <button type="button" className="compare-button" onClick={this.compare}>Compare Schedules</button>
+        <div>{this.state.commonPeriods}</div>
+      </div>
       );
   }
 }
